62aa6b27bd5b26bbb387240e58af9f842594fa7c
Jeffrey Larson <jeffothy@gmail.com>
2023-11-26 09:47:18 -0800

    Make code more succinct for readability

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,27 +1,18 @@
-let mediaPrintTabs = new Set();
+const mediaPrintTabs = new Set();
 
-chrome.action.onClicked.addListener(async (tab) => {
-  const currentTabId = tab.id;
+chrome.debugger.onDetach.addListener((source, reason) => {
+  mediaPrintTabs.delete(source.tabId);
+});
 
-  if (!mediaPrintTabs.has(currentTabId)) {
-    chrome.debugger.attach({ tabId: currentTabId }, '1.2', function () {
-      chrome.debugger.sendCommand(
-        { tabId: currentTabId },
-        'Emulation.setEmulatedMedia',
-        { media: "print" }
-      );
-    });
-
-    mediaPrintTabs.add(currentTabId);
-
-    chrome.debugger.onDetach.addListener(function (source, reason) {
-      const detachedTabId = source.tabId;
-      mediaPrintTabs.delete(detachedTabId);
-    });
+chrome.action.onClicked.addListener((currentTab) => {
+  const tab = currentTab.id;
+  if (!mediaPrintTabs.has(tab)) {
+    mediaPrintTabs.add(tab);
+    chrome.debugger.attach({ tabId: tab }, '1.2').then(
+      chrome.debugger.sendCommand({ tabId: tab }, 'Emulation.setEmulatedMedia', { media: "print" })
+    );
   } else {
-    if (mediaPrintTabs.has(currentTabId)) {
-      mediaPrintTabs.delete(currentTabId);
-      chrome.debugger.detach({ tabId: currentTabId });
-    }
+    mediaPrintTabs.delete(tab);
+    chrome.debugger.detach({ tabId: tab });
   }
 });
